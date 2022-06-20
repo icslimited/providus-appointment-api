d53156a6cb81d2cf5227d266158dbf4ecf3ac606
Jeremiah Ekundayo <dayoolapeju@outlook.com>
2022-06-20 19:39:10 +0100

    Setup migration for users, appointments, dept, cards, appointmentCards

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('userType');
+            $table->integer('contact');
+            $table->string('staffId');
+            $table->foreignId('department_id')->constrained('departments')->onDelete('cascade');
+            $table->string('branch');
+            $table->string('profilePicture');
+            $table->boolean('ableToLogin')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2022_06_20_164623_create_appointments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('appointments', function (Blueprint $table) {
+            $table->id();
+            $table->date('date');
+            $table->time('startTime');
+            $table->time('endTime');
+            $table->string('location');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->string('guestName');
+            $table->string('guestEmail');
+            $table->integer('guestContact');
+            $table->string('guestOrganization');
+            $table->text('reason');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('appointments');
+    }
+};
